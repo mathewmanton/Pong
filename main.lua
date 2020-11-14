0c33f996ef963bff40aace84535687e33936779f
MathewManton <mathewmanton@gmail.com>
2020-11-14 12:53:25 -0500

    -Start to the Pong Game -Prints text to the screen

--- main.lua
+++ main.lua
@@ -1,1 +1,22 @@
 
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+--[[Runs only once at the beginning of the game]]
+function love.load()
+  love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+      fullscreen = false,
+      resizable = false,
+      vsync = true
+  })
+end
+
+function love.draw()
+  love.graphics.printf(
+    'Hello Pong!', --text to render
+    0, --starting X (0 since we're going to center align)
+    WINDOW_HEIGHT / 2 - 6, --starting Y (halfway down the screen)
+    WINDOW_WIDTH, -- number of pixels to center
+    'center') -- alignment mode, can be 'center', 'left' or 'right' 
+end

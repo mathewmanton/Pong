fa67e64a644f4bf3ec12c44c32667a3455355b74
MathewManton <mathewmanton@gmail.com>
2020-11-14 13:19:48 -0500

    Added library Push Added a low res feel to the game Added functionality to exit game using Escape key

--- main.lua
+++ main.lua
@@ -1,22 +1,40 @@
 
+push = require 'push'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
---[[Runs only once at the beginning of the game]]
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+--Runs only once at the beginning of the game
 function love.load()
-  love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
       fullscreen = false,
       resizable = false,
       vsync = true
   })
 end
 
+function love.keypressed(key)
+  if key == 'escape' then
+    love.event.quit()
+  end
+end
+
+--Called after update
 function love.draw()
+  --begin rendering at virtual resolutino
+  push:apply('start')
+  
   love.graphics.printf(
     'Hello Pong!', --text to render
     0, --starting X (0 since we're going to center align)
-    WINDOW_HEIGHT / 2 - 6, --starting Y (halfway down the screen)
-    WINDOW_WIDTH, -- number of pixels to center
+    VIRTUAL_HEIGHT / 2 - 6, --starting Y (halfway down the screen)
+    VIRTUAL_WIDTH, -- number of pixels to center
     'center') -- alignment mode, can be 'center', 'left' or 'right' 
+  
+  --end rendering at virtual resolution
+  push:apply('end')
 end

221054103e23b7b63537f8424f205c4e0c2d8431
MathewManton <mathewmanton@gmail.com>
2020-11-14 18:33:34 -0500

    Added ball movement Added boundaries for the paddles

--- main.lua
+++ main.lua
@@ -13,6 +13,8 @@ PADDLE_SPEED = 200
 function love.load()
   love.graphics.setDefaultFilter('nearest', 'nearest')
   
+  math.randomseed(os.time())
+  
   smallFont = love.graphics.newFont('font.ttf', 8)
   scoreFont = love.graphics.newFont('font.ttf', 32)
   
@@ -27,6 +29,16 @@ function love.load()
   
   player1Y = 30
   player2Y = VIRTUAL_HEIGHT - 50
+  
+  --velocity and position variables for our ball when play starts
+  ballX = VIRTUAL_WIDTH / 2 - 2
+  ballY = VIRTUAL_HEIGHT / 2 - 2
+
+  -- math.random returns a random value between the left and right number
+  ballDX = math.random(2) == 1 and 100 or -100
+  ballDY = math.random(-50, 50) * 1.5
+
+  gameState = 'start'
 end
 
 --Runs every frame. delta in seconds since last frame
@@ -34,25 +46,40 @@ function love.update(dt)
     --player 1 movement
     if love.keyboard.isDown('w') then
       --add negative paddle speed to current Y scaled by deltatime
-      player1Y = player1Y + -PADDLE_SPEED * dt
+      player1Y = math.max(0, player1Y + -PADDLE_SPEED * dt)
     elseif love.keyboard.isDown('s') then
       --add positive paddle speed to current Y scaled by delta time
-      player1Y = player1Y + PADDLE_SPEED * dt
+      player1Y = math.min(VIRTUAL_HEIGHT - 20, player1Y + PADDLE_SPEED * dt)
     end
     
     --player 2 movement
     if love.keyboard.isDown('up') then
       --add negative paddle speed to current Y scaled by deltatime
-      player2Y = player2Y + -PADDLE_SPEED * dt
+      player2Y = math.max(0, player2Y + -PADDLE_SPEED * dt)
     elseif love.keyboard.isDown('down') then
       --add positive paddle speed to current Y scaled by delta time
-      player2Y = player2Y + PADDLE_SPEED * dt
+      player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y + PADDLE_SPEED * dt)
+    end
+    
+    if gameState == 'play' then
+      ballX = ballX + ballDX * dt
+      ballY = ballY + ballDY * dt
     end
 end
 
 function love.keypressed(key)
   if key == 'escape' then
     love.event.quit()
+  elseif key == 'enter' or key == 'return' then
+    if gameState == 'start' then
+      gameState = 'play'
+    else 
+      gameState = 'start'
+      ballX = VIRTUAL_WIDTH / 2 - 2
+      ballY = VIRTUAL_HEIGHT / 2 - 2
+
+      ballDX = math.random(2) == 1 and 100 or -100
+      ballDY = math.random(-50, 50) * 1.5
   end
 end
 
@@ -82,7 +109,7 @@ function love.draw()
   love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
   
   --ball
-  love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+  love.graphics.rectangle('fill', ballX, ballY, 4, 4)
   
   --end rendering at virtual resolution
   push:apply('end')

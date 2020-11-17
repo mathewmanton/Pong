e00f7aff861d72e759da3cc068b2e651ec863303
mathew manton <mathewmanton@hotmail.com>
2020-11-17 18:02:30 -0500

    Added a title a to the window Added FPS to the window

--- main.lua
+++ main.lua
@@ -16,6 +16,8 @@ PADDLE_SPEED = 200
 function love.load()
   love.graphics.setDefaultFilter('nearest', 'nearest')
   
+  love.window.setTitle('Pong')
+  
   math.randomseed(os.time())
   
   smallFont = love.graphics.newFont('font.ttf', 8)
@@ -102,6 +104,15 @@ function love.draw()
   ball:render()
   ball:render()
   
+  displayFPS()
+  
   --end rendering at virtual resolution
   push:apply('end')
 end
+
+function displayFPS()
+  love.graphics.setFont(smallFont)
+  love.graphics.setColor(0, 255, 0, 255)
+  love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 10, 10)
+end
+  

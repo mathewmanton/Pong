3ff8f9a43caaf332ac5215603e54c890e76de592
mathew manton <mathewmanton@hotmail.com>
2020-11-18 12:24:17 -0500

    Added sounds to paddle collision, boundary collision and scoring

--- main.lua
+++ main.lua
@@ -24,6 +24,12 @@ function love.load()
   largeFont = love.graphics.newFont('font.ttf', 16)
   scoreFont = love.graphics.newFont('font.ttf', 32)
   
+  sounds = {
+      ['paddle_hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+      ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+      ['wall_hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static')
+    }
+  
   push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
       fullscreen = false,
       resizable = false,
@@ -86,6 +92,7 @@ function love.update(dt)
         else
           ball.dy = math.random(10,150)
         end
+        sounds['paddle_hit']:play()
       end
       
       --collision between ball and player2
@@ -97,21 +104,25 @@ function love.update(dt)
           ball.dy = -math.random(10,150)
           ball.dy = math.random(10,150)
         end
+        sounds['paddle_hit']:play()
       end
       
       --collision between screen boundaries
       if ball.y <= 0 then
         ball.y = 0
         ball.dy = -ball.dy
+        sounds['wall_hit']:play()
       end
       if ball.y >= VIRTUAL_HEIGHT - 4 then
         ball.y = VIRTUAL_HEIGHT - 4
         ball.dy = -ball.dy
+        sounds['wall_hit']:play()
       end
       
       if ball.x < 0 then
         servingPlayer = 1
         player2Score = player2Score + 1
+        sounds['score']:play()
         if player2Score == 10 then
           winningPlayer = 2
           gameState = 'gameover'
@@ -120,10 +131,11 @@ function love.update(dt)
           ball:reset()
         end
       end
+      
       if ball.x > VIRTUAL_WIDTH then
         servingPlayer = 2
         player1Score = player1Score + 1
-        
+        sounds['score']:play()
         if player1Score == 10 then
           winningPlayer = 1 
           gameState = 'gameover'
